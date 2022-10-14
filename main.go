0b34386cae476522f9243fe8ef2bd490dec21a1d
Ahmad Malik <arma7x@live.com>
2022-10-14 22:55:04 +0800

    DONE: scrap financialtimes

--- main.go
+++ main.go
@@ -87,13 +87,11 @@ var RANKING_TYPES = map[string]string{
 }
 
 func GetCurrencies(c *gin.Context) {
-  // https://markets.ft.com/data/currencies/ajax/crossratesforselectedregion?group=Majors
   group := c.DefaultQuery("group", "Majors")
   if _, exist := CURRENCY[group]; !exist {
     group = "Majors"
   }
   url := fmt.Sprintf("https://markets.ft.com/data/currencies/ajax/crossratesforselectedregion?group=%s", group)
-  fmt.Println(url)
   res, err := http.Get(url)
   if err != nil {
     c.JSON(http.StatusInternalServerError, gin.H{
@@ -121,7 +119,10 @@ func GetCurrencies(c *gin.Context) {
   } else {
     var result map[string]interface{}
     if err := json.Unmarshal(b, &result); err != nil {
-      fmt.Println(err)
+      c.JSON(http.StatusInternalServerError, gin.H{
+        "error": err.Error(),
+        "url": url,
+      })
     } else {
       if _, exist := result["html"]; !exist {
         c.JSON(http.StatusInternalServerError, gin.H{
@@ -171,20 +172,230 @@ func GetCurrencies(c *gin.Context) {
 }
 
 func GetCommodities(c *gin.Context) {
-  // https://markets.ft.com/data/commodities
+  url := "https://markets.ft.com/data/commodities"
+  res, err := http.Get(url)
+  if err != nil {
+    c.JSON(http.StatusInternalServerError, gin.H{
+      "error": err.Error(),
+      "url": url,
+    })
+    return
+  }
+
+  defer res.Body.Close()
+  if res.StatusCode != 200 {
+    c.JSON(res.StatusCode, gin.H{
+      "error": res.StatusCode,
+      "url": res.Status,
+    })
+    return
+  }
+
+  if doc, err := goquery.NewDocumentFromReader(res.Body); err != nil {
+    c.JSON(http.StatusInternalServerError, gin.H{
+      "error": err.Error(),
+      "url": url,
+    })
+  } else {
+    c.JSON(http.StatusOK, gin.H{
+      "data": map[string]any{
+        "energy": extractCommodities(doc.Find("#energy-panel")),
+        "metals": extractCommodities(doc.Find("#metals-panel")),
+        "agricultureandlumber": extractCommodities(doc.Find("#agricultureandlumber-panel")),
+      },
+    })
+  }
+}
+
+func extractCommodities(panel *goquery.Selection) map[string]any {
+  var headers []string
+  var results []map[string]any
+  table := panel.Find("table")
+  table.Children().Each(func(i int, tbody *goquery.Selection) {
+    if (i == 0) {
+      tbody.Children().Each(func(j int, tr *goquery.Selection) {
+        tr.Children().Each(func(k int, td *goquery.Selection) {
+          if (k == 4) {
+            var spans []string
+            td.Children().First().Children().Each(func(j int, span *goquery.Selection) {
+              spans = append(spans, strings.TrimSpace(span.Text()))
+            })
+            headers = append(headers, strings.Join(spans, "@"))
+          } else {
+            headers = append(headers, strings.TrimSpace(td.Text()))
+          }
+        })
+      })
+    } else if (i == 1) {
+      tbody.Children().Each(func(j int, tr *goquery.Selection) {
+        t := make(map[string]any)
+        tr.Children().Each(func(k int, td *goquery.Selection) {
+          if (k == 0) {
+            t[headers[k]] = []string{strings.TrimSpace(td.Children().First().Text()), strings.TrimSpace(td.Children().Last().Text())}
+          } else if (k == 1) {
+            full := strings.TrimSpace(td.Text())
+            code := strings.TrimSpace(td.Children().Last().Text())
+            full =  strings.TrimSpace(strings.Replace(full, code, "", 1))
+            t[headers[k]] = []string{full, code}
+          } else if (k == 2) {
+            full := strings.TrimSpace(td.Text())
+            code := strings.TrimSpace(td.Find("span").Find("span").Text())
+            full =  strings.TrimSpace(strings.Replace(full, code, "", 1))
+            t[headers[k]] = []string{full, code}
+          } else if (k == 3) {
+            if val, exist := td.Children().Last().Attr("style"); exist {
+              t[headers[k]] = strings.TrimSpace(val)
+            } else {
+              t[headers[k]] = false
+            }
+          } else if (k == 4) {
+            hl := make(map[string][]string)
+            keys := map[string]string{
+              "low": ".mod-ui-range-bar__container__label--lo",
+              "high": ".mod-ui-range-bar__container__label--hi",
+            }
+            for key := range keys {
+              dom := td.Find(keys[key])
+              if (strings.TrimSpace(dom.Text()) != "") {
+                var s []string
+                dom.Children().Each(func(j int, span *goquery.Selection) {
+                  s = append(s, strings.TrimSpace(span.Text()));
+                })
+                hl[key] = s
+              } else {
+                hl[key] = []string{}
+              }
+            }
+            t[headers[k]] = hl
+          }
+        })
+        results = append(results, t)
+      })
+    }
+  })
+  return map[string]any{
+    "header": headers,
+    "data": results,
+  }
 }
 
 func GetBondsAndRates(c *gin.Context) {
-  // https://markets.ft.com/data/bonds
+  url := "https://markets.ft.com/data/bonds"
+  res, err := http.Get(url)
+  if err != nil {
+    c.JSON(http.StatusInternalServerError, gin.H{
+      "error": err.Error(),
+      "url": url,
+    })
+    return
+  }
+
+  defer res.Body.Close()
+  if res.StatusCode != 200 {
+    c.JSON(res.StatusCode, gin.H{
+      "error": res.StatusCode,
+      "url": res.Status,
+    })
+    return
+  }
+
+  if doc, err := goquery.NewDocumentFromReader(res.Body); err != nil {
+    c.JSON(http.StatusInternalServerError, gin.H{
+      "error": err.Error(),
+      "url": url,
+    })
+  } else {
+    c.JSON(http.StatusOK, gin.H{
+      "data": map[string]any{
+        "interbankratesovernight": extractBondsAndRates(doc.Find("#interbankratesovernight-panel")),
+        "officialinterestrates": extractBondsAndRates(doc.Find("#officialinterestrates-panel")),
+        "marketrates": extractBondsAndRates(doc.Find("#marketrates-panel")),
+      },
+    })
+  }
+}
+
+func extractBondsAndRates(panel *goquery.Selection) map[string]any {
+  var headers []string
+  var results []map[string]string
+  table := panel.Find("table")
+  table.Children().Each(func(i int, tbody *goquery.Selection) {
+    if (i == 0) {
+      tbody.Children().Each(func(j int, tr *goquery.Selection) {
+        tr.Children().Each(func(k int, td *goquery.Selection) {
+          headers = append(headers, strings.TrimSpace(td.Text()))
+        })
+      })
+    } else if (i == 1) {
+      tbody.Children().Each(func(j int, tr *goquery.Selection) {
+        t := make(map[string]string)
+        tr.Children().Each(func(k int, td *goquery.Selection) {
+          t[headers[k]] = strings.TrimSpace(td.Text())
+        })
+        results = append(results, t)
+      })
+    }
+  })
+  return map[string]any{
+    "header": headers,
+    "data": results,
+  }
 }
 
 func GetGovernmentBondsSpreads(c *gin.Context) {
-  // https://markets.ft.com/data/bonds/government-bonds-spreads
+  url := "https://markets.ft.com/data/bonds/government-bonds-spreads"
+  res, err := http.Get(url)
+  if err != nil {
+    c.JSON(http.StatusInternalServerError, gin.H{
+      "error": err.Error(),
+      "url": url,
+    })
+    return
+  }
+
+  defer res.Body.Close()
+  if res.StatusCode != 200 {
+    c.JSON(res.StatusCode, gin.H{
+      "error": res.StatusCode,
+      "url": res.Status,
+    })
+    return
+  }
+
+  if doc, err := goquery.NewDocumentFromReader(res.Body); err != nil {
+    c.JSON(http.StatusInternalServerError, gin.H{
+      "error": err.Error(),
+      "url": url,
+    })
+  } else {
+    var headers []string
+    var results []map[string]string
+    table := doc.Find("table")
+    table.Children().Each(func(i int, tbody *goquery.Selection) {
+      if (i == 0) {
+        tbody.Children().Each(func(j int, tr *goquery.Selection) {
+          tr.Children().Each(func(k int, td *goquery.Selection) {
+            headers = append(headers, strings.TrimSpace(td.Text()))
+          })
+        })
+      } else if (i == 1) {
+        tbody.Children().Each(func(j int, tr *goquery.Selection) {
+          t := make(map[string]string)
+          tr.Children().Each(func(k int, td *goquery.Selection) {
+            t[headers[k]] = strings.TrimSpace(td.Text())
+          })
+          results = append(results, t)
+        })
+      }
+    })
+    c.JSON(http.StatusOK, gin.H{
+      "header": headers,
+      "data": results,
+    })
+  }
 }
 
 func GetEquities(c *gin.Context) {
-  // https://markets.ft.com/data/equities/ajax/updatemarketmovers?rankingType=percentgainers&rankingSet=SP500
-  // https://markets.ft.com/data/equities/ajax/updatemarketmovers?rankingType=%s&rankingSet=%s
   rankingType := c.DefaultQuery("rankingType", "percentgainers")
   if _, exist := RANKING_TYPES[rankingType]; !exist {
     rankingType = "percentgainers"
@@ -194,7 +405,6 @@ func GetEquities(c *gin.Context) {
     rankingSet = "SP500"
   }
   url := fmt.Sprintf("https://markets.ft.com/data/equities/ajax/updatemarketmovers?rankingType=%s&rankingSet=%s", rankingType, rankingSet)
-  fmt.Println(url)
   res, err := http.Get(url)
   if err != nil {
     c.JSON(http.StatusInternalServerError, gin.H{
@@ -222,7 +432,10 @@ func GetEquities(c *gin.Context) {
   } else {
     var result map[string]interface{}
     if err := json.Unmarshal(b, &result); err != nil {
-      fmt.Println(err)
+      c.JSON(http.StatusInternalServerError, gin.H{
+        "error": err.Error(),
+        "url": url,
+      })
     } else {
       if _, exist := result["data"]; !exist {
         c.JSON(http.StatusInternalServerError, gin.H{
@@ -279,6 +492,7 @@ func GetEquities(c *gin.Context) {
               }
             });
             c.JSON(http.StatusOK, gin.H{
+              "header": headers,
               "data": result,
             })
           }
